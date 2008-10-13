aa70e64cb6721428303a189f7fc8c42b26b9f680
Sü¾¼ren Hü¾¼jsgaard <sorenh@agrsci.dk>
2008-10-13 20:14:22 +0000

    version 3.5

--- data/dietox.R
+++ data/dietox.R
@@ -3,3 +3,5 @@ dietox$Evit <- factor(dietox$Evit);
 dietox$Cu <- factor(dietox$Cu);
 dietox$Litter <- factor(dietox$Litter)
 dietox$Pig <- factor(dietox$Pig)
+levels(dietox$Evit) <- c("Evit000","Evit100","Evit200")
+levels(dietox$Cu) <- c("Cu000","Cu035","Cu175")
