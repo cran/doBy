ebce8e242878370f79a006d7f686fc86fbf20762
Sü¾¼ren Hü¾¼jsgaard <sorenh@agrsci.dk>
2011-05-08 05:46:20 +0000

    version 4.3.1

--- data/dietox.R
+++
@@ -1,7 +0,0 @@
-dietox <- read.table("dietox.txt",header=TRUE)
-dietox$Evit <- factor(dietox$Evit);
-dietox$Cu <- factor(dietox$Cu);
-dietox$Litter <- factor(dietox$Litter)
-dietox$Pig <- factor(dietox$Pig)
-levels(dietox$Evit) <- c("Evit000","Evit100","Evit200")
-levels(dietox$Cu) <- c("Cu000","Cu035","Cu175")
