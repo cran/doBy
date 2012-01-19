e38e2d5ab5f609b0a4eecad0fc98ded1f184dcde
Sü¾¼ren Hü¾¼jsgaard <sorenh@mail.dk>
2012-01-19 12:08:35 +0000

    version 4.5.0

--- R/popMeans.R
+++ R/popMeans.R
@@ -2,43 +2,33 @@ popMeans <- function(object, effect=NULL, at=NULL, only.at=TRUE, engine="esticon
   UseMethod("popMeans")
 }
 
-popMeans.lm <- function(object, effect=NULL, at=NULL, only.at=TRUE, engine="esticon", ...){
+popMeans.default <- function(object, effect=NULL, at=NULL, only.at=TRUE, engine="esticon", ...){
   cl  <- match.call()
   mm  <- popMatrix(object, effect=effect, at=at, only.at=only.at)
   ans <- do.call(engine, list(object, mm,...))
 
   if (engine=="esticon"){
     attributes(ans)[c("grid","at")] <- attributes(mm)[c("grid","at")]
-    attr(ans,"X") <- mm
+    attr(ans,"X")    <- mm
     attr(ans,"call") <- cl
-    class(ans)    <- c("popMeans", "conMeans", "data.frame")
+    class(ans) <- c("popMeans", "conMeans", "data.frame")
   }
   ans
 }
 
-popMeans.mer <- function(object, effect=NULL, at=NULL, only.at=TRUE, engine="esticon", ...){
-  cl  <- match.call()
-  mm  <- popMatrix(object, effect=effect, at=at, only.at=only.at)
-  ans <- do.call(engine, list(object, mm,...))
-
-  if (engine=="esticon"){
-    attributes(ans)[c("grid","at")] <- attributes(mm)[c("grid","at")]
-    attr(ans,"X") <- mm
-    attr(ans,"call") <- cl
-    class(ans)    <- c("popMeans", "conMeans", "data.frame")
-  }
-  ans
+popMeans.lme <- function(object, effect=NULL, at=NULL, only.at=TRUE, engine="esticon", ...){
+  cl <- match.call()
+  lm.pseudo <- lm(object,data=object$data)
+  cl[[2]]   <- lm.pseudo
+  cl[[1]]   <- as.name("popMeans.default")
+  eval(cl)
 }
-
-lsMeans     <- popMeans
-lsMeans.lm  <- popMeans.lm
-lsMeans.mer <- popMeans.mer
 
 linMeans <- function(object, at=NULL, engine="esticon", ...){
   UseMethod("linMeans")
 }
 
-linMeans.lm <- function(object, at=NULL, engine="esticon", ...){
+linMeans.default <- function(object, at=NULL, engine="esticon", ...){
   cl  <- match.call()
   mm  <- linMatrix(object, at=at)
   ans <- do.call(engine, list(object, mm,...))
@@ -52,12 +42,20 @@ linMeans.lm <- function(object, at=NULL, engine="esticon", ...){
   ans
 }
 
+linMeans.lme <- function(object, at=NULL, engine="esticon", ...){
+  cl <- match.call()
+  lm.pseudo <- lm(object,data=object$data)
+  cl[[2]]   <- lm.pseudo
+  cl[[1]]   <- as.name("linMeans.default")
+  eval(cl)
+}
+
 
 summary.conMeans <- function(object,...){
   print(object)
-
   cat("Call:\n")
   print(attr(object,"call"))
   cat("Contrast matrix:\n")
   summary(attr(object,"X"))
 }
+
